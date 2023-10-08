c963b6ea7ab4a1ac2d222921de36745bcb5e24af
Dominik Jahn <dominik-jahn@outlook.de>
2023-10-08 23:02:25 +0200

    add missing files

---
+++ BlazorPWA2/Modal/YesNoModal.razor.cs
@@ -0,0 +1,17 @@
+﻿using Blazored.Modal;
+using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorPWA2.Modal;
+
+public partial class YesNoModal
+{
+    [CascadingParameter] BlazoredModalInstance BlazoredModal { get; set; } = default!;
+    private async void Ok(){
+        await BlazoredModal.CloseAsync(ModalResult.Ok());
+    }
+
+    private async void Cancel(){
+        await BlazoredModal.CloseAsync(ModalResult.Cancel());
+    }
+}
